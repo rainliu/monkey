5bde2414a030aa866eea11c5843819c9c1bdbdec
Rain Liu <yuliu@outlook.com>
2019-06-25 23:15:52 -0700

    pass unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -9,56 +9,91 @@ mod parser_test;
 
 pub struct Parser<'a> {
     lexer: Peekable<Lexer<'a>>,
+    cur_token: Token,
+    errors: Vec<String>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(lexer: Lexer<'a>) -> Self {
-        Parser {
+        let mut p = Parser {
             lexer: lexer.peekable(),
-        }
+            cur_token: Token::EOF,
+            errors: Vec::new(),
+        };
+
+        p.next_token();
+
+        p
+    }
+
+    pub fn errors(&self) ->&[String] {
+        &self.errors
     }
 
     pub fn parse_program(&mut self) -> Program {
         let mut program = Program::new();
 
-        while self.lexer.peek() != None && self.lexer.peek() != Some(&Token::EOF) {
+        while self.cur_token != Token::EOF {
             let stmt = self.parse_statement();
             if let Some(stmt) = stmt {
                 program.statements.push(stmt);
             }
+            self.next_token();
         }
 
         program
     }
 
     fn parse_statement(&mut self) -> Option<Statement> {
-        match self.lexer.peek() {
-            Some(&Token::LET) => self.parse_statement_let(),
+        match self.cur_token {
+            Token::LET => self.parse_statement_let(),
             _ => None,
         }
     }
 
     fn parse_statement_let(&mut self) -> Option<Statement> {
-        self.lexer.next().unwrap();
-        let ident = match self.lexer.peek() {
-            Some(&Token::IDENT(_)) => self.lexer.next().unwrap(),
-            _ => return None,
-        };
         match self.lexer.peek() {
-            Some(&Token::ASSIGN) => self.lexer.next(),
-            _ => return None,
+            Some(&Token::IDENT(_)) => self.next_token(),
+            Some(t) => {
+                self.errors.push(format!("expected next token to be IDENT, got {:?} instead", t));
+                return None
+            },
+            None => {
+                self.errors.push(format!("expected next token to be IDENT, got None instead"));
+                return None
+            },
+        };
+
+        let ident = self.cur_token.clone();
+
+        match self.lexer.peek() {
+            Some(&Token::ASSIGN) => self.next_token(),
+            Some(t) => {
+                self.errors.push(format!("expected next token to be ASSIGN, got {:?} instead", t));
+                return None
+            },
+            None => {
+                self.errors.push(format!("expected next token to be ASSIGN, got None instead"));
+                return None
+            },
         };
 
         //TODO:
-        while let Some(t) = self.lexer.next() {
-            if t == Token::SEMICOLON {
-                break;
-            }
+        while self.cur_token != Token::SEMICOLON && self.cur_token != Token::EOF {
+            self.next_token();
         }
 
         Some(Statement::Let(
             Identifier { token: ident.clone() },
             Expression::Ident(Identifier { token: ident }),
         ))
+    }
+
+    fn next_token(&mut self) {
+        if let Some(token)  = self.lexer.next() {
+            self.cur_token = token;
+        }else {
+            self.cur_token = Token::EOF;
+        }
     }
 }
--- src/parser/parser_test.rs
+++ src/parser/parser_test.rs
@@ -12,11 +12,26 @@ fn test_statement_let() {
     let mut p = Parser::new(l);
 
     let program = p.parse_program();
+    check_parser_errors(&p);
     assert_eq!(program.statements.len(), 3);
 
     for (tt, stmt) in tests.iter().zip(program.statements.iter()) {
         assert_eq!(is_statement_let(stmt, tt), true);
     }
+}
+
+#[test]
+fn test_statement_let_errors() {
+    let input = "let x 5;
+    let  = 10;
+    let  838383";
+
+    let l = Lexer::new(input);
+    let mut p = Parser::new(l);
+
+    let program = p.parse_program();
+    check_parser_errors(&p);
+    assert_eq!(program.statements.len(), 0);
 }
 
 fn is_statement_let(s: &Statement, name: &str) -> bool {
@@ -30,5 +45,17 @@ fn is_statement_let(s: &Statement, name: &str) -> bool {
             true
         },
         //_ => false,
+    }
+}
+
+fn check_parser_errors(p: &Parser) {
+    let errors = p.errors();
+    if errors.len() == 0 {
+        return;
+    }
+
+    println!("parser has {} errors", errors.len());
+    for error in errors{
+        println!("parser error: {}", error);
     }
 }
